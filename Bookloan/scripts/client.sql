bf152ca17c1fd994afb6d6d2d222f1c17ebe0ad9
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-08-14 21:50:30 -0300

    add properties book and client

--- Bookloan/scripts/client.sql
+++ Bookloan/scripts/client.sql
@@ -3,7 +3,10 @@ CREATE TABLE Client
 	Id integer identity(1,1),
 	UniqueKey uniqueidentifier default newId() not null,
 	Name varchar(80) not null,
+	Lastname varchar(80) not null,
 	SocialNumber varchar(15) not null,
+	Email varchar(120) not null,
+	Phone varchar(15),
 	Active bit
 );
 
--- Bookloan/src/MGG.Bookloan.Domain/Entities/Book.cs
+++ Bookloan/src/MGG.Bookloan.Domain/Entities/Book.cs
@@ -5,6 +5,6 @@ namespace MGG.Bookloan.Domain.Entities
     public class Book : Entity
     {
         public string Title { get; set; }
-        public bool Active { get; set; }
+        public string Author { get; set; }
     }
 }

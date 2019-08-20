0e400951c69554fe5dceea2675d61b0b04e54550
Mateus Gonçalves Geracino <mateusgoncalvesgeracino@hotmail.com>
2019-08-19 22:03:12 -0300

    add method login

---
+++ Bookloan/src/MGG.Bookloan.Services/ViewModels/Response/LoginResponseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
+
+namespace MGG.Bookloan.Services.ViewModels.Response
+{
+    public class LoginResponseViewModel
+    {
+        public string SociaNumber { get; set; }
+        public string Name { get; set; }
+        public string Token { get; set; }
+
+        [JsonIgnore]
+        public ValidationResult ValidationResult { get; set; }
+    }
+}
